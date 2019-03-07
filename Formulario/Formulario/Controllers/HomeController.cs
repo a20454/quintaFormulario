2e1b5fc4cdacb855cf988300cb1f2f435ef6e6c4
Fullapple1991 <Fullapple1991@LAPTOP-M4FMFGBG>
2019-03-07 16:21:16 +0000

    16:21

---
+++ Formulario/Formulario/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Formulario.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.oreo = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string nome, int idade)
+        {
+            //criar a mensagem de resposta
+            string mensagem = "voce chama se "+nome+" e tem "+idade+" anos.";
+            //criar o 'contetor' que levara a mensagem para a view
+            ViewBag.oreo = mensagem;
+            //invoca a view
+            return View();
+        }
+    }
+}
